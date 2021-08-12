abcea97d5d003811fc63380eadbfa7e30ecbb383
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-12 11:11:17 +0900

    ログイン状態によるHeaderコンポーネントの表示変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 import * as React from 'react'
 import Link from 'next/link'
+import {auth} from '../firebase'
 
 const Header = ({ pathname }) => (
   <header>
     <Link href="/">
       <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
     </Link>{' '}
-    <Link href="/about">
-      <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
-    </Link>
+
+    {auth.currentUser===null? '': 
+      <Link href="/about">
+        <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
+      </Link>
+    }
   </header>
 )
 

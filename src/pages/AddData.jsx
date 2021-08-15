54341dfa87eb222a1620a0b91fae0eca2927f4b9
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-16 08:56:56 +0900

    material-uiの導入とusersコレクションデータの取得

---
+++ src/pages/AddData.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import firebase from "firebase/app";
+
+import { db } from '../firebase'
+import { Button } from '@material-ui/core';
+
+export default function AddData() {
+  const handleClickFetchButton=()=>{
+    db.collection('users').doc('GmMn9BiFZAaaY0Mu1N4l').get().then((doc)=>{
+      console.log("Document data:", doc.data());
+    })
+    console.log('Fetchボタンが押下されました')
+  }
+  return (
+    <>
+      <h1>FireeStore用とのデータのやり取り確認用のページです。</h1>
+      {/* <button onClick={handleClickFetchButton}>取得</button> */}
+      {/* <Button>Default</Button> */}
+      <Button color="primary" onClick={handleClickFetchButton}>firestoreからデータの取得</Button>
+      {/* <Button color="secondary">Secondary</Button>
+      <Button disabled>Disabled</Button>
+      <Button href="#text-buttons" color="primary">
+        Link
+      </Button> */}
+    </>
+  )
+}

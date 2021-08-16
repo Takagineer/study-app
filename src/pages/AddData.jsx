499b6af6359ddfbc591cd2963f5fb098f2e1b8ca
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-08-16 10:33:48 +0900

    firebaseからのデータの取得

--- src/pages/AddData.jsx
+++ src/pages/AddData.jsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import firebase from "firebase/app";
 
 import { db } from '../firebase'
 import { Button } from '@material-ui/core';
 
 export default function AddData() {
+  const [users,setUsers] = useState([])
+  
   const handleClickFetchButton=async()=>{
-    const doc = await db.collection('users').doc('GmMn9BiFZAaaY0Mu1N4l').get()
-    console.log(doc.data())
+    // ドキュメントの取得
+    // const doc = await db.collection('users').doc('GmMn9BiFZAaaY0Mu1N4l').get()
+    // console.log(doc.data())
+    // ドキュメントの取得
 
-    console.log('Fetchボタンが押下されました')
+    // コレクションの取得
+    const snapshot = await db.
+    collection('users')
+    .where('age','<=',20)
+    .limit(1)
+    .get()
+    snapshot.forEach(doc=>{
+      console.log(doc.id,'=>',doc.data()) 
+    })
+    // コレクションの取得
   }
   return (
     <>
       <h1>FireeStore用とのデータのやり取り確認用のページです。</h1>
-      {/* <button onClick={handleClickFetchButton}>取得</button> */}
-      {/* <Button>Default</Button> */}
       <Button color="primary" onClick={handleClickFetchButton}>firestoreからデータの取得</Button>
-      {/* <Button color="secondary">Secondary</Button>
-      <Button disabled>Disabled</Button>
-      <Button href="#text-buttons" color="primary">
-        Link
-      </Button> */}
     </>
   )
 }
